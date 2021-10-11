d8955bc6b88b7f58a5c1c5b7643852808174a241
Vsevolod Melnik <vsevolodmelnik@MacBook-Air-Vsevolod.local>
2021-10-11 16:41:01 +0300

    Initial commit, storyboard is deleted

---
+++ vkmelnikPW3/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  vkmelnikPW3
+//
+//  Created by Vsevolod Melnik on 11.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
